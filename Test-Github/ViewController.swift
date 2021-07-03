52dc67fe0596b33f4b01a7c84e337a7d25bf1ee2
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2021-07-03 06:10:59 +0500

    Revert "1st commit in 5th-Branch"
    This reverts commit d5a7e2790dfcd454d02728f51b7af14f5c4b5b30.
    
    # Conflicts:
    #	Test-Github/ViewController.swift

--- Test-Github/ViewController.swift
+++ Test-Github/ViewController.swift
@@ -28,9 +28,12 @@ class ViewController: UIViewController {
     
     //1st commit in 4th-Branch
     //2nd commit in 4th-Branch
+<<<<<<< HEAD
     
     //1st commit in 5th-Branch
     //2nd commit in 5th-Branch
+=======
+>>>>>>> parent of d5a7e27 (1st commit in 5th-Branch)
 
 }
 
